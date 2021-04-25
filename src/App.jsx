35f97267dd8951a7644a8151583b650ed64ebf6d
JinSo <kimjinso@qq.com>
2021-04-25 13:23:52 +0800

    初始化配置

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App...
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
+}
